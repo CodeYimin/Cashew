596ce07c3d37153b314858d68fd7efa6ab224fd0
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2024-05-03 02:14:08 -0400

    Setup server for Nginx

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,24 +2,16 @@ import cors from "cors";
 import express from "express";
 import session from "express-session";
 // import { db } from "./db";
-import fs from "fs";
-import https from "https";
 import routes from "./routes";
 
-const PORT = process.env.PORT || 4000;
-const SSL_CERT_PATH = process.env.SSL_CERT_PATH;
-const SSL_KEY_PATH = process.env.SSL_KEY_PATH;
-
-const httpsOptions = {
-  cert: fs.readFileSync(SSL_CERT_PATH || ""),
-  key: fs.readFileSync(SSL_KEY_PATH || ""),
-};
-
 async function start() {
+  const PORT = process.env.PORT || 4000;
+  const ROOT = process.env.ROOT || "/";
+
   const app = express();
   app.use(
     cors({
-      origin: ["https://codeyimin.github.io", "http://localhost:3000"],
+      origin: ["https://codeyimin.github.io"],
       credentials: true,
     }),
     session({
@@ -29,15 +21,11 @@ async function start() {
       saveUninitialized: false,
     })
   );
-  app.use("/api", routes);
+  app.use(ROOT, routes);
 
-  https.createServer(httpsOptions, app).listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  // app.listen(PORT, () => {
-  //   console.log(`Server is running on port ${PORT}`);
-  // });
 
   // console.log(await db.user.findMany());
 }
